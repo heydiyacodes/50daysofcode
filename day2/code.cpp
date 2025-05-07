0a5a8934338de5714930dee9d9625a70d93a2ddb
heydiyacodes <diya.codesdev@gmail.com>
2025-05-07 19:21:01 +0530

    Update code.cpp

--- day2/code.cpp
+++ day2/code.cpp
@@ -1,1 +1,23 @@
+//day2: 1st May
+//Problem: Leetcode 876. Middle of the Linked List
 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int cnt = 0;
+        ListNode* temp = head;
+
+        while (temp != nullptr) {
+            cnt++;
+            temp = temp->next;
+        }
+
+        int middle = cnt / 2;
+        temp = head;
+        for (int i = 0; i < middle; i++) {
+            temp = temp -> next;
+        }
+
+        return temp;
+    }
+};

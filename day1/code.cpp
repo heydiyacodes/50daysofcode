b86344f3ac882056349becea479d2dbef140c1d5
heydiyacodes <diya.codesdev@gmail.com>
2025-05-07 19:18:04 +0530

    Update code.cpp

--- day1/code.cpp
+++ day1/code.cpp
@@ -1,1 +1,18 @@
+//day1: 30th Apr
+//Problem: Leetcode 206. Reverse Linked List
 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;       
+        ListNode* curr = head;          
+
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next; 
+            curr->next = prev;               
+            prev = curr;                     
+            curr = nextNode;                 
+        }
+        return prev;  // prev becomes the new head
+    }
+};

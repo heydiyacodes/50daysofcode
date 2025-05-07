766bf914fdce58784289c3fc3ca8b0b59eec9e27
heydiyacodes <diya.codesdev@gmail.com>
2025-05-07 19:27:14 +0530

    Update code.cpp

--- day4/code.cpp
+++ day4/code.cpp
@@ -1,1 +1,21 @@
+//day4: 3rd April
+//Problem: Leetcode 141. Linked List Cycle
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) return false;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};
 

1ace0f7337e430c037be31209901dc2912679d89
heydiyacodes <diya.codesdev@gmail.com>
2025-05-07 19:29:08 +0530

    Update code.cpp

--- day5/code.cpp
+++ day5/code.cpp
@@ -1,1 +1,34 @@
+//day5: 4th April
+//Problem: Leetcode 234. Palindrome Linked List
 
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+      
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+        while (curr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        ListNode* first = head;
+        ListNode* second = prev;  
+        while (second) {
+            if (first->val != second->val) return false;
+            first = first->next;
+            second = second->next;
+        }
+        return true;
+    }
+};

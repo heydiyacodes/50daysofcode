8059ff7cc358d55416ce44362ab57ecb9e9bca7b
heydiyacodes <diya.codesdev@gmail.com>
2025-05-07 19:37:11 +0530

    Update code.cpp

--- day7/code.cpp
+++ day7/code.cpp
@@ -1,1 +1,34 @@
+//day7: 6th April
+//Problem: Leetcode 61. Rotate List
 
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+
+        // Step 1: Count the length of the list
+        int length = 1;
+        ListNode* tail = head;
+        while (tail->next) {
+            tail = tail->next;
+            length++;
+        }
+
+        // Step 2: Make the list circular
+        tail->next = head;
+
+        // Step 3: Find the new tail (length - k % length - 1) and new head
+        k = k % length;
+        int stepsToNewTail = length - k;
+        ListNode* newTail = head;
+        for (int i = 1; i < stepsToNewTail; ++i) {
+            newTail = newTail->next;
+        }
+
+        // Step 4: Break the circle and return new head
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+
+        return newHead;
+    }
+};
